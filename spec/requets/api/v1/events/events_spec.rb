5daa44f00950c88b31b6d2bac1c83d2d8c702ded
Dennis Zelada <denniszelada@gmail.com>
2015-10-12 14:01:30 -0300

    Add get request for events

---
+++ spec/requets/api/v1/events/events_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'GET /v1/events/:id', type: :request do
+  it 'returns an event by :id' do
+    event = create(:event)
+
+    get "/v1/events/#{event.id}"
+
+    expect(response_json).to eq (
+      {
+        'address' => event.address,
+        'ended_at' => event.ended_at,
+        'id' => event.id,
+        'lat' => event.lat,
+        'lon' => event.lon,
+        'name' => event.name,
+        'started_at' => event.started_at.as_json,
+        'owner' => {
+          'id' => event.owner.id
+        }
+      }
+    )
+  end
+end
